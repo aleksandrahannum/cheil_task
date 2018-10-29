bf22e4f20ac3d864e7ed14e0877a77cdb354f5a9
Aleksandra Hannum <aleksandrahannum@gmail.com>
2018-10-29 15:01:21 +0100

    mobile menu

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
-  $('#toggleButton').click(function(){
-    $('.nav_list_items').stop().slideToggle();
-  });
-    
-    
-}); 
+$(document).ready(function () {
+
+    $(function () {
+
+        if ($(window).scrollTop() >= 20) {
+            $('.nav_bar').addClass('scroll');
+        };
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >= 20) {
+                $('.nav_bar').addClass('scroll');
+            } else {
+                $('.nav_bar').removeClass('scroll');
+            }
+
+        });
+    });
+
+    $('#toggleButton').click(function () {
+        $('.nav_list_items').stop().slideToggle();
+    });
+
+
+
+
+});
